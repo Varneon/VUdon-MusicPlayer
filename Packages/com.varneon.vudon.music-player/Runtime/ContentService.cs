3861452b06722f6d5755eaeb5a331b502524d6e2
Varneon <me@varneon.com>
2023-02-12 00:13:55 -0800

    Add content service enum

---
+++ Packages/com.varneon.vudon.music-player/Runtime/ContentService.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.MusicPlayer.Enums
+{
+    public enum ContentService
+    {
+        [InspectorName("YouTube")]
+        YouTube,
+        VRCDN,
+        Soundcloud,
+        Twitch,
+        Vimeo,
+        Mixcloud,
+        [InspectorName("NicoNico")]
+        NicoNico,
+        Youku,
+        GoogleDrive,
+        FacebookVideo,
+        Other
+    }
+}
