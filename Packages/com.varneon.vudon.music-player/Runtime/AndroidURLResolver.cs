cc56bf2610573ce1d1db1d46fc79ea6301937b18
Varneon <me@varneon.com>
2023-02-12 00:14:37 -0800

    Add Android URL resolver enum

---
+++ Packages/com.varneon.vudon.music-player/Runtime/AndroidURLResolver.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.MusicPlayer.Enums
+{
+    public enum AndroidURLResolver
+    {
+        None,
+        [InspectorName("Jinnai (t-ne.x0.to)")]
+        Jinnai,
+        Custom
+    }
+}
