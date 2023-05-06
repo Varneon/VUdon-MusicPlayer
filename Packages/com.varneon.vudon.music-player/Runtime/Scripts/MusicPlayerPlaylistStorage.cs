e57bdd24df20bb6880a92b75f72ab60fd8262160
Varneon <me@varneon.com>
2023-05-06 06:07:50 -0700

    Add MusicPlayerPlaylistStorage

---
+++ Packages/com.varneon.vudon.music-player/Runtime/Scripts/MusicPlayerPlaylistStorage.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Varneon.VUdon.MusicPlayer
+{
+    public class MusicPlayerPlaylistStorage : MonoBehaviour
+    {
+        public List<SongPlaylist> Playlists = new List<SongPlaylist>();
+    }
+}
