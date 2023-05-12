d994d027e2bd2c32ae34b9e69166d6cd673c683c
Varneon <me@varneon.com>
2023-05-12 05:20:34 -0700

    Add tooltips and custom names to MusicPlayerMode

--- Packages/com.varneon.vudon.music-player/Runtime/MusicPlayerMode.cs
+++ Packages/com.varneon.vudon.music-player/Runtime/MusicPlayerMode.cs
@@ -1,8 +1,15 @@
-﻿namespace Varneon.VUdon.MusicPlayer.Enums
+﻿using UnityEngine;
+
+namespace Varneon.VUdon.MusicPlayer.Enums
 {
     public enum MusicPlayerMode
     {
+        [Tooltip("Only one audio source is supported, recommended for 2D audio with audio filters, e.g. Reverb, Lowpass")]
+        [InspectorName("Unity (VRCUnityVideoPlayer)")]
         Unity,
+
+        [Tooltip("Support for multiple speakers, suitable for making realistic speaker sources")]
+        [InspectorName("AVPro (VRCAVProVideoPlayer)")]
         AVPro
     }
 }
