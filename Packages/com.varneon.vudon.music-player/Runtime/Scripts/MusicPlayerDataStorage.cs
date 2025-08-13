53c75c140c7a833fcde9f31b47426eed8abedcf4
Varneon <me@varneon.com>
2025-08-13 03:19:18 -0700

    Make playlist storage a generic data storage
    Use the new component to store any data that is not compatible with Udon, not just playlists used for the player

---
+++ Packages/com.varneon.vudon.music-player/Runtime/Scripts/MusicPlayerDataStorage.cs
@@ -0,0 +1,20 @@
+﻿#if !COMPILER_UDONSHARP
+
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Varneon.VUdon.MusicPlayer
+{
+    [AddComponentMenu("")]
+    [DisallowMultipleComponent]
+    public class MusicPlayerDataStorage : MonoBehaviour
+    {
+        public List<SongPlaylist> Playlists = new List<SongPlaylist>();
+
+        public UnityMusicPlayerSpeaker UnitySpeaker;
+
+        public List<AVProMusicPlayerSpeaker> AVProSpeakers = new List<AVProMusicPlayerSpeaker>();
+    }
+}
+
+#endif
